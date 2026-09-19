a71f2b352dd6c5635a54ebd57c3b3b496624a328
dasha-byte <dasha.rum2006@gmail.com>
2026-09-19 10:32:33 +0300

    Feat: add function in mainpage.js - when user turn on app and see loading screen, change mainpage.css and index.html - add image pig, loading line and view of mobile phone

--- mainpage.js
+++ mainpage.js
@@ -2,10 +2,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     const splashScreen = document.getElementById("splash-screen");
     const mainScreen = document.getElementById("main-screen");
-    // таймер на 3 секунды
-    setTimeout(() => {
-        // мы плавно скрываем заставку и показываем главный экран
-        splashScreen.classList.add("hidden");
-        // показываем затем главный экран
-        mainScreen.classList.remove("hidden"); }, 3000);
+    // полоса загрузки
+    const loadingLine = document.getElementById('loading-line');
+    const loadingText = document.getElementById('loading-text');
+    let procent = 0;
+    // запускаем счетчик процентов
+    const a = setInterval(() => {
+        if (procent >= 100) {
+            // останавливаем загрузку когда будет 100%
+            clearInterval(a);
+            // 3 секунды загрузка
+            setTimeout(() => {
+                // скрываем экран загрузки и показываем главный экран
+                splashScreen.classList.add("hidden");
+                mainScreen.classList.remove("hidden");
+            }, 300);
+        } else {
+            procent += 1; //увеличиваем на 1 процент
+            // обноваляем элементы
+            if (loadingLine && loadingText) {
+                loadingLine.style.width = procent + '%';
+                loadingText.innerText = procent + '%';
+            }
+        }
+    }, 25) // 25 милисекунд на каждый шаг
 });
