10b135cda1c174536a1646c2244fd44fcdacea88
dasha-byte <dasha.rum2006@gmail.com>
2026-09-22 00:52:13 +0300

    feat: increase World,change stars, delete auto scrolling Map

--- my-app/src/screens/WorldScreen.js
+++ my-app/src/screens/WorldScreen.js
@@ -2,12 +2,12 @@ import React, {useEffect, useRef} from 'react';
 import { StyleSheet,View,ImageBackground,Animated,ScrollView,Text,TouchableOpacity,Dimensions } from 'react-native';
 
 // размер глобуса
-const PLANET_SIZE = 280;
+const PLANET_SIZE = 330;
 
 export default function WorldScreen({navigation}) {
     // анимация звезд
-    const fadeAnim1 = useRef(new Animated.Value(0.4)).current;
-    const fadeAnim2 = useRef(new Animated.Value(0.8)).current;
+    const fadeAnim1 = useRef(new Animated.Value(0.3)).current;
+    const fadeAnim2 = useRef(new Animated.Value(0.5)).current;
     //вращение земли анимация
     const mapScrollAnim = useRef(new Animated.Value(0)).current;
 
@@ -15,32 +15,35 @@ export default function WorldScreen({navigation}) {
         //мерцание звездочек
         Animated.loop(
             Animated.sequence([
-                Animated.timing(fadeAnim1,{toValue:1,duration:1500,useNativeDriver:true}),
-                Animated.timing(fadeAnim1,{toValue:0.3,duration:2000,useNativeDriver:true})
+                Animated.timing(fadeAnim1,{toValue:0.9,duration:2500,useNativeDriver:true}),
+                Animated.timing(fadeAnim1,{toValue:0.2,duration:2000,useNativeDriver:true})
             ])
         ).start();
+
         
         Animated.loop(
             Animated.sequence([
-                Animated.timing(fadeAnim2, {toValue:0.2,duration:1200,useNativeDriver:true}),
-                Animated.timing(fadeAnim2, {toValue:0.9,duration:1400,useNativeDriver:true})
+                Animated.timing(fadeAnim2, {toValue:0.1,duration:1800,useNativeDriver:true}),
+                Animated.timing(fadeAnim2, {toValue:0.8,duration:2200,useNativeDriver:true})
             ])
         ).start();
+
         //вращение глобуса в разные стороны
-        Animated.loop(
-            Animated.sequence([
-                Animated.timing(mapScrollAnim, {
-                    toValue: -300,
-                    duration: 20000,
-                    useNativeDriver: true,
-                }),
-                Animated.timing(mapScrollAnim, {
-                    toValue: 0,
-                    duration: 0,
-                    useNativeDriver: true,
-                })
-            ])
-        ).start();
+        //ребенок сам может крутить землю
+        // Animated.loop(
+        //     Animated.sequence([
+        //         Animated.timing(mapScrollAnim, {
+        //             toValue: -300,
+        //             duration: 20000,
+        //             useNativeDriver: true,
+        //         }),
+        //         Animated.timing(mapScrollAnim, {
+        //             toValue: 0,
+        //             duration: 0,
+        //             useNativeDriver: true,
+        //         })
+        //     ])
+        // ).start();
     }, []);
 
     return (
@@ -49,10 +52,10 @@ export default function WorldScreen({navigation}) {
             <ImageBackground source={require('../../assets/Darksky.png')} style={styles.background} />
         </Animated.View>
     
-        <Animated.View style={[styles.starLayer,{opacity:fadeAnim2, transform:[{scale:1.05},{rotate:'90deg'}]}]}>
+        <Animated.View style={[styles.starLayer,{opacity:fadeAnim2, transform:[{scale:1.1},{rotate:'45deg'}]}]}>
             <ImageBackground source={require('../../assets/Darksky.png')} style={styles.background} />
         </Animated.View>
-        {/* Земля */}
+        {/* Земля - масштабирование*/}
         <ScrollView
             maximumZoomScale={3.0}
             minimumZoomScale={1.0}
@@ -60,38 +63,75 @@ export default function WorldScreen({navigation}) {
             showsVerticalScrollIndicator={false}
             contentContainerStyle={styles.centerSpace}
             >
+                {/* ручное вращение а не автоматическое */}
                 <View style={styles.planetCircle}>
-                    <Animated.Image
-                        source={require('../../assets/mapWorld.png')}
-                        style={[
-                            styles.mapImage,
-                            {transform: [{translateX: mapScrollAnim}]}
-                        ]}
-                    resizeMode="cover"
-                    />
-                    <View style={styles.planetOverlay} />
+                    <ScrollView horizontal showsHorizontalScrollIndicator={false} bounces={false}>
+                        <Animated.Image
+                            source={require('../../assets/mapWorld.png')}
+                            style={styles.mapImage}
+                                // {transform: [{translateX: mapScrollAnim}]}
+                            resizeMode="cover"/>
+                    </ScrollView>
+                    <View style={styles.planetOverlay} pointerEvents='none'/>
                 </View>
             </ScrollView>
-
+            {/* Меню */}
+            <View style={styles.bottomBar}>
+                <TouchableOpacity style={styles.actionButton} onPress={() => setOpenMenu('tasks')}>
+                  <Text style={styles.actionEmoji}>📋</Text>
+                  <Text style={styles.actionText}>Задания</Text>
+                        </TouchableOpacity>
+                        {/* КНОПКА С ЗЕМЛЕЙ путешествие по странам */}
+                        <TouchableOpacity 
+                          style={styles.actionButton} 
+                          onPress={() => navigation.navigate('World')}>
+                          <Text style={styles.actionEmoji}>🌍</Text>
+                          <Text style={styles.actionText}>Мир</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.actionButton} onPress={() => setOpenMenu('room')}>
+                          <Text style={styles.actionEmoji}>🏠</Text>
+                          <Text style={styles.actionText}>Комната</Text>
+                </TouchableOpacity>
+            </View>
         </ImageBackground>
+                  
     );
 }
 const styles = StyleSheet.create({
-    background: {flex: 1, width:'100%',height: '100%',backgroundColor:'#0c0728'},
+    background: {
+        flex: 1, 
+        width:'100%',
+        height: '100%',
+        backgroundColor:'#0c0728'
+    },
   starLayer: {...StyleSheet.absoluteFillObject },
-  centerSpace: {flex: 1, alignItems: 'center', justifyContent:'center'},
+  centerSpace: {
+    flex: 1, alignItems: 'center', 
+    justifyContent:'center'
+    },
   planetCircle: {
     width: PLANET_SIZE,
     height: PLANET_SIZE,
     borderRadius: PLANET_SIZE / 2,
     overflow: 'hidden',
     backgroundColor: '#1c3d5a',
-    borderWidth: 3,
-    borderColor: '#3b82f6',
+    borderWidth: 4,
+    borderColor: '#42a5f5',
     position: 'relative',
-  },
-  mapImage: {width: PLANET_SIZE * 3, height:PLANET_SIZE },
-  planetOverlay: {...StyleSheet.absoluteFillObject,backgroundColor:'rgba(255, 255, 255, 0.08)',borderRadius:PLANET_SIZE/2 },
+    //тень
+    shadowColor: '#3b82f6',
+    shadowOpacity: 0.4,
+    shadowRadius: 15,
+    elevation: 10,
+    },
+  mapImage: {
+    width: PLANET_SIZE * 2.5, 
+    height:PLANET_SIZE 
+    },
+  planetOverlay: {...StyleSheet.absoluteFillObject,
+    backgroundColor:'rgba(255, 255, 255, 0.08)',
+    borderRadius:PLANET_SIZE/2 
+    },
   bottomBar: {
     position: 'absolute',
     bottom: 0,
@@ -104,10 +144,17 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255,255,255,0.92)',
     borderTopLeftRadius: 24,
     borderTopRightRadius: 24,
-  },
-  actionButton: {alignItems: 'center', minWidth: 80 },
+     },
+  actionButton: {
+    alignItems: 'center', 
+    minWidth: 80 
+    },
   actionEmoji: {fontSize: 26 },
-  actionText: {fontSize: 13, color: '#000', marginTop: 4 },
+  actionText: {
+    fontSize: 13, 
+    color: '#000', 
+    marginTop: 4 
+    },
   activeIconCircle: {
     width: 44,
     height: 44,
